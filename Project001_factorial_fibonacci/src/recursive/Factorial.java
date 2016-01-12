1cf9fa845fca9564412669519a426d99fc2f77a1
atafs <atafs@iscte-iul.pt>
2016-01-12 11:53:39 +0000

    start

---
+++ Project001_factorial_fibonacci/src/recursive/Factorial.java
@@ -0,0 +1,19 @@
+package recursive;
+
+public class Factorial {
+
+	//Calculo de Factorial recursivo
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++){
+			System.out.println("O factorial de " +i+ " é "+ Factorial.calFact(i));
+		}
+	}
+	
+	private static int calFact(int n){
+		if (n <= 1)
+			return 1;
+		else 
+			return n * calFact(n -1);
+	}
+}
